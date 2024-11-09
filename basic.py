245aaa474e1a54f1af2cbb08b32573ea1f48dc36
dinesh25255 <dineshbhatt0707@gmail.com>
2024-11-09 21:15:15 +0545

    python and data-science

---
+++ basic.py
@@ -0,0 +1,38 @@
+#first we print a hello world program 
+
+print("my name is dinesh bhatta . i read in class 14 . i live in ")
+
+print("i read in class 14")
+
+# when we write this code then that type of the error is comming 
+
+# print(dinesh bhatt)
+
+#integer
+print(7)
+
+#Boolean
+print(True)
+
+#decimal
+print(7.5)
+
+print("hello" , 1,4.5,True) #it can print all seperated by a space
+
+print("hello",1,4.5,True, sep='/') # that condition time  output is 
+
+# when we print same name two times 
+
+print("hello")
+print("dinesh")  
+
+#it can give output in different line 
+
+
+#we have one solution  we print name in same line 
+print("hello" , end='-')
+print('dinesh')
+
+
+
+
---
+++ data_types.py
@@ -0,0 +1,37 @@
+#integer
+print(4)
+
+#decimal
+print(1e308)  #python can handle long large integer
+
+#boolean
+print('true')
+print('false')
+
+
+#text
+print("hello world")
+
+#complex
+print(5+6j)
+
+
+#list
+print({1,2,3,3})
+
+#tuple
+print((1,2,3,4))
+
+#set
+print({1,2,3,4})
+
+
+#dictionary
+print({'name': 'dinesh'})
+
+
+#type
+type(3)
+#type is used  to print a datatype 
+type(3,4,5)
+
