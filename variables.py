611a9b4201da404021d818b4619a0f114d030024
dinesh25255 <dineshbhatt0707@gmail.com>
2024-11-09 22:26:07 +0545

    python

---
+++ variables.py
@@ -0,0 +1,38 @@
+#in c c++ we writw that type of the code 
+
+# int name ="dinesh bhatt" #in  c c++  we write a code that type 
+
+name = 'dines bhatt'
+print(name)
+
+# in this  way we print name in python programming language 
+
+# in short python is automatically understand what is data type
+
+a = 2
+b= 3
+print(a+b)
+
+#dynamic typing
+a=5
+
+#dynamic Binding
+a=5
+print(a)
+a='dinesh'
+print(a)
+#in dynamic Binding there is no fix variable
+
+#static Binding
+# int a=5 #throw out of the program
+
+#python can  support Dynamic Binding
+
+a=1
+b= 3
+c=4
+print(a,b,c)
+
+
+#we can create a multiple variable
+
