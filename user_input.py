be1f36c5c40375b3d8270b8d7b382ebbfa0b9972
dinesh25255 <dineshbhatt0707@gmail.com>
2024-11-10 20:31:43 +0545

    python and data science

---
+++ user_input.py
@@ -0,0 +1,37 @@
+
+
+#take input from user and store them in a variable
+fnum = input('enter first number')
+sSum=input('enter a second number')
+print(fnum,sSum)
+
+# add the two variable 
+result = fnum+sSum
+
+#print the result
+
+print(result)
+
+#literals
+
+#binary
+a= 0b1010
+print(a)
+
+#octal
+a= 0o310
+print(a)
+
+#float literal
+
+float_1=10.5
+float_2=1.5e2
+float_3=1.5e-3
+
+#complex literal
+
+x=3.14
+
+print(a,b,c,d)
+print(float_1,float_2,float_3)
+print(x,x.imag,x.real)
