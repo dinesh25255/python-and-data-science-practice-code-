9f53a4842fc7583999bc403937b3259a0b745cd1
dinesh25255 <dineshbhatt0707@gmail.com>
2024-11-13 20:53:30 +0545

    python programming

---
+++ Second video topic/modules in python/all modules.py
@@ -0,0 +1,22 @@
+import math
+#all function are there
+
+math.floor(6,7)
+
+
+#keyword modules
+
+import keyboard
+print(keyboard.kwlist)
+
+
+# random modules
+
+import random
+print(random.randint(1,100))
+
+#datetime modules
+
+print(datetime.datdtime.now())
+
+
---
+++ Second video topic/modules in python/loop.py
@@ -0,0 +1,11 @@
+#while loop example  example write a program to print the table
+
+
+number = int(input('enter the number'))
+
+i=1
+while I<11:
+    print(number*i)
+    i+=1
+
+    
