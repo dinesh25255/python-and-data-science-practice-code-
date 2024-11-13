9b5c1ee94ac7129af9d445a095a27897279d35ad
dinesh25255 <dineshbhatt0707@gmail.com>
2024-11-13 20:57:09 +0545

    python

---
+++ Second video topic/Atm_drive.py
@@ -0,0 +1,25 @@
+#menu drive program Atm
+
+menu = input("""
+             Hi, how can i help you
+             1.enter 1 for pin charge 
+             2.enter 2 for balance charge
+             3. enter 3 for withdraw
+             4. enter 4 for exit
+             
+             
+             
+             
+             """)
+
+if menu == '1':
+    print('pin charge')
+
+elif menu == '2':
+    print('Balance:')
+
+else:
+    print('exit')
+
+    
+
---
+++ Second video topic/calculator.py
@@ -0,0 +1,21 @@
+#menu drive program 
+
+fnum = int(input('enter the 1 st number'))
+snum = int(input('enter the 2 st number'))
+
+op = input('enter the operation ')
+
+if op == '+':
+    print(fnum+snum)
+
+elif op == '-':
+    print(fnum-snum)
+
+elif op == '*':
+    print(fnum * snum)
+
+else:
+    print(fnum/snum)
+
+
+
